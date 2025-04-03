c6fecefd15c309ebc5ca033e952310ac88047392
EbunoluwaAjani <ajaniebunolu@gmail.com>
2025-04-03 20:53:56 +0100

    Add files via upload

--- api_task1/aws_estree.py
+++ api_task1/aws_estree.py
@@ -3,17 +3,20 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 import awswrangler as wr
-#from api2_task import extract
+from api2_task import extract
 
 
 load_dotenv()
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-file_name = os.getenv('file_path')
-df = pd.read_csv('api_doc2.csv')
-df2 = pd.read_csv('remotejobsrole.csv')
-df3 = pd.read_csv('football_competition.csv')
-df4 = pd.read_csv('randomuserprofiles.csv')
+API_KEY = os.getenv('API_KEY')
+url1 = 'https://content.guardianapis.com/search?page=1&q="nigeria"&from-date=2025-01-01&api-key={API_KEY}'
+df = extract(url1)
+
+#df = pd.read_csv('api_doc2.csv')
+#df2 = pd.read_csv('remotejobsrole.csv')
+#df3 = pd.read_csv('football_competition.csv')
+#df4 = pd.read_csv('randomuserprofiles.csv')
 
 session = boto3.session.Session(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -27,7 +30,7 @@ wr.s3.to_parquet(
     mode='append',
     dataset=True
 )
-
+'''
 wr.s3.to_parquet(
     df=df2,
     path= "s3://apitest-by-ebun/api_tasks/remotejobroles",
@@ -50,7 +53,7 @@ wr.s3.to_parquet(
     boto3_session=session,
     mode='append',
     dataset=True)
-
+'''
 
 
 
